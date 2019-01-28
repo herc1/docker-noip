e5fc43edcb0700d99cd84c9ffd1db4d9a63f67db
herc1 <hnstoyanov@gmail.com>
2019-01-28 21:52:59 +0000

    no -S option!!!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-noip2 -S -d
-sleep 3600
-#while true; do
-# if [ $(pgrep noip2 | wc -l) -eq 0 ]
-# then
-#  echo "noip is not running. Starting daemon"
-#  noip2 -S -d
-# else
-#  sleep 5
-# fi
-# done
+noip2 -d
+while true; do
+ if [ $(pgrep noip2 | wc -l) -eq 0 ]
+ then
+  echo "noip is not running. Starting daemon"
+  noip2 -S -d
+ else
+  sleep 5
+ fi
+ done
